ce8962b4f16270c170668a7c71ffb13703c6e70b
Jincy99 <60498991+Jincy99@users.noreply.github.com>
2020-02-16 16:32:18 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,30 @@
+#To print in Multiple lines
+print("Jincy  P S\n Parappallil houde \n Kayarthadka post \n Kalenja Village")
+
+#To print 8 blank lines
+print("\n\n\n\n\n\n\n\n")
+print(8*"\n")
+
+#To print "Artificail intelligence" 7 times
+print(7*" Artificail intelligence\n")
+
+#To print integer number
+num=int(input("Enter a number: "))
+print(num)
+
+#To print floating point numbers
+fnum=float(input("Entet the number: "))
+print(fnum)
+
+#To print name
+name=input("Tell us your name: ")
+print(name)
+
+#using '' And ''' ''' to print
+print('haiii')
+print('''hello world''')
+
+#using end=''
+print('jincy',end='746');print('ps',end='@');print('gmail',end='.')
+print('com')
+
---
+++ temperature.py
@@ -0,0 +1,4 @@
+#to convert temperature from degree celsious to farenheit
+cel=float(input("Enter the temperature in o'C: "))
+faren=cel*1.8+32
+print("The temperature in terms of Farenheit is:  ",faren)
