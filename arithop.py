9dd6469a59653731b901d590fb1b0a0d7ac9d3b3
Jincy99 <60498991+Jincy99@users.noreply.github.com>
2020-03-11 22:21:25 +0530

    Add files via upload

---
+++ arithop.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 15:48:54 2020
+
+@author: realestore
+"""
+
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number:"))
+print("sum=",num1+num2)
+print("Difference=",num1-num2)
+print("product=",num1*num2)
